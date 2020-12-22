0ec0a9ddbaf774efa57a3f7d33568b079914bf99
DyAvr <55398221+DyAvr@users.noreply.github.com>
2020-12-22 12:33:19 +0300

    global search

---
+++ MafiaForum/ViewModels/Search/SearchResultViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MafiaForum.ViewModels.Post;
+
+namespace MafiaForum.ViewModels.Search
+{
+    public class SearchResultViewModel
+    {
+        public IEnumerable<PostListingViewModel> Posts { get; set; }
+        public string SearchQuery { get; set; }
+        public bool EmptySearchResults { get; set; }
+    }
+}
